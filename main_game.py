196d92b4146f3bb6380443c36360c8ef1f034bf7
24online24 <chiuzbaianrares20@gmail.com>
2022-05-25 00:23:31 +0300

    Fixing conflicts

--- main_game.py
+++ main_game.py
@@ -12,13 +12,15 @@ HEIGHT = 480
 
 global highscore, clicked, root, prev_score, _exit, file, file1
 
+
 def create_box(image):
     image_height, image_width = image.shape[0], image.shape[1]
     box_width = random.randrange(WIDTH // 6, WIDTH // 4)
-    box_height = int(box_width * 1.3) 
+    box_height = int(box_width * 1.3)
     x = random.randrange(0, image_width - box_width)
     y = random.randrange(0, image_height - box_height)
     return((x, y, box_width, box_height))
+
 
 def detect_hand(image, area, detector):
     image = detector.find_hands(image)
@@ -31,39 +33,45 @@ def detect_hand(image, area, detector):
             return False
     return True
 
+
 def show_highscore():
     ms.showinfo("Highscore", f"Highscore = {highscore}")
+
 
 def show_prev_score():
     global prev_score
     ms.showinfo("Previous Score", f'Previous Score = {prev_score}')
+
 
 def onClick():
     global root, _exit
     _exit = True
     root.destroy()
 
+
 def start_game():
     global clicked, root
     clicked = True
     root.destroy()
-    
+
+
 def place_buttons():
     global prev_score, highscore
-    
-    file = open("/home/norby/Coding/College/Practica/MAIN_/highscore.txt", 'r+')
-    highscore = int(file.read())
-    file1 = open("/home/norby/Coding/College/Practica/MAIN_/prev_score.txt", 'r+')
-    prev_score = int(file1.read())
-    
-    b1 = tk.Button(root, text = "Show Highscore", command = show_highscore)
-    b2 = tk.Button(root, text = "Show previous score", command = show_prev_score)
-    b3 = tk.Button(root, text = "Start game", command = start_game)
 
-    b2.place(x = HEIGHT // 2 + 10, y = HEIGHT // 2 - HEIGHT // 3)
-    b1.place(x = HEIGHT // 2 + 23, y = HEIGHT // 2 - HEIGHT // 4.5)
-    b3.place(x = HEIGHT // 2 + 36, y = HEIGHT // 1.93 - HEIGHT // 8)
-    
+    file = open("highscore.txt", 'r+')
+    highscore = int(file.read())
+    file1 = open("prev_score.txt", 'r+')
+    prev_score = int(file1.read())
+
+    b1 = tk.Button(root, text="Show Highscore", command=show_highscore)
+    b2 = tk.Button(root, text="Show previous score", command=show_prev_score)
+    b3 = tk.Button(root, text="Start game", command=start_game)
+
+    b2.place(x=HEIGHT // 2 + 10, y=HEIGHT // 2 - HEIGHT // 3)
+    b1.place(x=HEIGHT // 2 + 23, y=HEIGHT // 2 - HEIGHT // 4.5)
+    b3.place(x=HEIGHT // 2 + 36, y=HEIGHT // 1.93 - HEIGHT // 8)
+
+
 def menu():
     global root
     root = tk.Tk(className=" Gesture Game")
@@ -73,14 +81,15 @@ def menu():
     root.protocol("WM_DELETE_WINDOW", onClick)
     root.mainloop()
 
+
 def game():
     global highscore, prev_score, clicked, _exit
-    
-    file = open("/home/norby/Coding/College/Practica/MAIN_/highscore.txt", 'r+')
-    file1 = open("/home/norby/Coding/College/Practica/MAIN_/prev_score.txt", 'r+')
-    
-    capture = cv2.VideoCapture(0)  
-    
+
+    file = open("highscore.txt", 'r+')
+    file1 = open("prev_score.txt", 'r+')
+
+    capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
     capture.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
     capture.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
     box_now = found = False
@@ -91,35 +100,33 @@ def game():
     score = -1
     tracker = HandTracking.HandDetector()
     timer = random.randint(12, 15)
-    
+
     while True and clicked:
         success, image = capture.read()
-        
+
         if timer <= 0.1:
             if score > highscore:
                 file.seek(0)
                 file.truncate()
                 file.writelines(str(score))
-                
-            
+
             file1.seek(0)
             file1.truncate()
             file1.writelines(str(score))
-            
+
             break
-        
+
         if box_now == False:
             score += 1
             coordinates = create_box(image)
             box_now = True
             found = False
-            
+
             if first_box:
                 first_box = not first_box
             else:
                 timer = random.randint(7, 10)
 
-            
         cv2.rectangle(image, (coordinates[0], coordinates[1]),
                       (coordinates[0] + coordinates[2], coordinates[1] + coordinates[3]), (255, 0, 255), 5)
         found = detect_hand(image, coordinates, tracker)
@@ -133,10 +140,9 @@ def game():
 
         cv2.rectangle(image, (0, 0), (125, 130), (128, 128, 128), -1)
 
-        
         elapsed = round(current_time - start_time, 2)
         speed = round(score / elapsed, 2)
-        
+
         cv2.putText(image, 'Highscore: ' + str(highscore), (10, 30),
                     cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
         cv2.putText(image, 'Score: ' + str(score), (10, 60),
@@ -145,40 +151,38 @@ def game():
                     cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
         cv2.putText(image, 'Timer: ' + str(round((timer := timer - 0.1), 2)), (10, 120),
                     cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
-        
+
         cv2.imshow("Image", image)
         cv2.waitKey(1)
-        
+
         if cv2.getWindowProperty("Image", cv2.WND_PROP_VISIBLE) < 1:
             _exit = True
             break
-        
+
     cv2.destroyAllWindows()
-        
+
     file.close()
     file1.close()
 
+
 funcmap = {
-    1 : menu,
-    2 : game
+    1: menu,
+    2: game
 }
 
 if __name__ == '__main__':
-    global _exit
-    
     _exit = False
-    
+
     choice = 1
     while True:
         choice = 1
         funcmap[choice]()
-        
+
         if _exit:
-         break
-        
+            break
+
         choice = 2
         funcmap[choice]()
-        
+
         if _exit:
-         break
-        
+            break
